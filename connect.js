6feb13ee6114c56ea35f37180d5b32a917492397
ikerherran <117441950+ikerherran@users.noreply.github.com>
2026-02-16 11:24:11 +0100

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,24 +1,24 @@
 const { MongoClient } = require('mongodb');
-const fs = require('fs');
 
-async function runReport() {
-    const client = new MongoClient("mongodb://localhost:27017");
+async function datuakAtera() {
+    const client = new MongoClient("mongodb://localhost:27017");  // Zerbitzarira konexioa
 
     try {
         await client.connect();
-        const db = client.db('offices_2026'); 
-        const collection = db.collection('offices_temp_humidity');
+        const db = client.db('offices_2026');  // datubasearen izena
+        const collection = db.collection('offices_temp_humidity'); // bildumaren izena
 
-        // The aggregation: save all data in an array (results)
+        // Orain query-a exekutatu eta array baten (results izena jarri diogu) gordeko dira emaitzak
         const results = await collection.aggregate([
             { $group: { _id: "$officeId", avgTemp: { $avg: "$temperature" } } },
             { $sort: { avgTemp: -1 } }
-        ]).toArray(); 
-        console.log(results);
+        ]).toArray();  
+        console.log(results);  // pantailan array-aren edukia erakutsi
     } catch (err) {
         console.error("Error:", err);
     } finally {
         await client.close();
     }
 }
-runReport();
+// Orain sortu berri dugun funtzioa exekutatuko dugu.
+datuakAtera();
