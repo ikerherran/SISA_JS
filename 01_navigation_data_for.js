8c7d9f81ceafd8c2745bc4af71184a3521f21064
ikerherran <117441950+ikerherran@users.noreply.github.com>
2026-03-25 14:18:47 +0100

    for

---
+++ 01_navigation_data_for.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require('mongodb');
+//const fs = require('fs');
+//------------------------------------------------------------------------------
+async function datuakAtera() {
+    const client = new MongoClient("mongodb://localhost:27017");  // Zerbitzarira konexioa
+
+    try {
+        await client.connect();
+        const db = client.db('web-navigation');  // datubasearen izena
+        const collection = db.collection('traffic_logs'); // bildumaren izena
+
+        // Orain query-a exekutatu eta array baten (results izena jarri diogu) gordeko dira emaitzak
+        const results = await collection.aggregate([
+            { $match: { visitedWeb: "amazon.com"} },
+            { $project: {_id:0,userIP:1,visitedWeb:1,durationMinutes:{$dateDiff:{startDate:"$startDateTime", endDate: "$endDateTime",unit: "minute"}} }}
+        ]).toArray();  
+
+        let batura = 0;
+        for (let i = 0; i < results.length; i++) {
+            let minutes = results[i].durationMinutes;
+            batura += minutes;
+        }
+        console.log("Batura:", batura);
+        
+      //  console.log(results);  // pantailan array-aren edukia erakutsi
+    } catch (err) {
+        console.error("Error:", err);
+    } finally {
+        await client.close();
+    }
+}
+//------------------------------------------------------------------------
+// Orain sortu berri dugun funtzioa exekutatuko dugu.
+datuakAtera();
